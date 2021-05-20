d3bca520d041df14eaede104a93344caddd45cdd
lukaswoodtli <woodtli.lukas@gmail.com>
2021-05-20 11:31:01 +0200

    Add filtering functionality

--- src/main/webapp/app/entities/product/product.module.ts
+++ src/main/webapp/app/entities/product/product.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'app/shared/shared.module';
-import { ProductComponent } from './list/product.component';
+import { FilterPipe, ProductComponent } from './list/product.component';
 import { ProductDetailComponent } from './detail/product-detail.component';
 import { ProductUpdateComponent } from './update/product-update.component';
 import { ProductDeleteDialogComponent } from './delete/product-delete-dialog.component';
@@ -9,7 +9,7 @@ import { ProductRoutingModule } from './route/product-routing.module';
 
 @NgModule({
   imports: [SharedModule, ProductRoutingModule],
-  declarations: [ProductComponent, ProductDetailComponent, ProductUpdateComponent, ProductDeleteDialogComponent],
+  declarations: [ProductComponent, ProductDetailComponent, ProductUpdateComponent, ProductDeleteDialogComponent, FilterPipe],
   entryComponents: [ProductDeleteDialogComponent],
 })
 export class ProductModule {}
